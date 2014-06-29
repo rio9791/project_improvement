286c801625a96a876520facd7d9e59adbb1ec374
Rio Dermawan <rio.dermawan57@gmail.com>
2014-06-29 16:55:03 +0700

    Initialize commit

---
+++ project_improvement/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectImprovement::Application.config.secret_key_base = '45e203c683bd7defee656b79e22b4c60906f3d1e52e7983100d2c04865e81322ba18455559a955037435ea87811293d34e32c3ee1f31f964cda24ccc7f09d38e'
